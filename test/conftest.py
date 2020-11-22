7541f18e9ef754d27d466b34e3b4b7e8b0e7d6c0
Robert Gwynn-Jones <gwynnr@gmail.com>
2020-11-22 16:51:41 +1100

    Add useful fixtures to tests

--- test/conftest.py
+++ test/conftest.py
@@ -8,6 +8,7 @@ ROOT_DIR = os.path.dirname(TEST_DIR)
 #PACKAGE_DIR = os.path.join(ROOT_DIR, "batcher")
 sys.path.append(ROOT_DIR)
 
+from batcher.batch_controller import BatchController
 from batcher.job import Job
 from batcher.simple_batch_processor import SimpleBatchProcessor
 
@@ -16,9 +17,38 @@ def job():
     return Job(job_fn=lambda: 2)
 
 @pytest.fixture
+def make_job():
+    def _make_job():
+        return Job(job_fn=lambda: 2)
+
+    yield _make_job
+
+@pytest.fixture
 def job_result(job: Job):
     return JobResult(job=job)
 
 @pytest.fixture
+def make_job_result(make_job):
+    def _make_job_result():
+        return JobResult(job=make_job())
+
+    yield _make_job_result
+
+@pytest.fixture
 def batch_processor():
     return SimpleBatchProcessor()
+
+@pytest.fixture
+def make_batch_controller(batch_processor):
+    controllers = []
+
+    def _make_batch_controller(batch_size, batch_interval):
+        return BatchController(
+            batch_size=batch_size, batch_interval=batch_interval, batch_processor=batch_processor)
+
+    yield _make_batch_controller
+
+    # shutdown all controllers
+    for controller in controllers:
+        print ("Shutting down controller")
+        controller.shutdown()
